f170dcd5a9a1d581a8a4c55e7b6e770f65ec6dc6
kittysquee <kittysquee@gmail.com>
2017-03-21 10:32:40 +0000

    Deducts money from balance (Step 7)

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,8 +2,23 @@ class Oystercard
 
   attr_reader :balance
 
+  MAXIMUM_BALANCE = 90
+
   def initialize
     @balance = 0
   end
+
+  def top_up(money)
+    fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if money + balance > MAXIMUM_BALANCE
+    self.balance += money
+  end
+
+  def deduct_money(money)
+    self.balance -= money
+  end
+
+  private
+
+  attr_writer :balance
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,20 @@ describe Oystercard do
   it 'has a balance of 0' do
     expect(subject.balance).to eq 0
   end
+
+  it 'adds money to the balance' do
+    expect{ subject.top_up 10 }.to change{ subject.balance }.by 10
+  end
+
+  it "raises an error when maximum balance is exceeded" do
+    maximum_balance = Oystercard::MAXIMUM_BALANCE
+    subject.top_up(maximum_balance)
+    expect{subject.top_up(1)}.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+  end
+
+  it 'deducts money from card' do
+    subject.top_up(20)
+    expect{subject.deduct_money(5)}.to change{subject.balance}.by -5
+  end
+
 end
