68a4f9bba88a444afc03072bffc59603fdf43ab9
Colin Turner <colinturner34@gmail.com>
2017-03-23 10:46:46 +0000

    Failing test for journey class exit station

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,5 +1,14 @@
 class Journey
 
+attr_reader :entry_station
+
+
+def start(station)
+  self.entry_station = station
+end
+
+private
+attr_writer :entry_station
 
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -3,12 +3,21 @@ require 'journey'
 describe Journey do
 
   subject(:journey) {described_class.new}
-  let(:station) {double :station}
+  let(:start_station) {double :station}
+  let(:finish_station) {double :station}
 
   describe '#start' do
     it 'assigns an entry station' do
-      journey.start(station)
-      expect(journey.entry_station).to eq station
+      journey.start(start_station)
+      expect(journey.entry_station).to eq start_station
+    end
+  end
+
+  describe '#finish' do
+    it 'assigns an exit station' do
+      journey.start(start_station)
+      journey.finish(finish_station)
+      expect(journey.entry_station).to be_nil
     end
   end
 
