8a6b998cfe6356eeae38a01faa6a455b9611124b
Stephanie Crampin <steph.crampin@gmail.com>
2017-03-23 10:35:21 +0000

    added entry station test for journey

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,1 +1,15 @@
+require 'journey'
 
+describe Journey do
+
+  subject(:journey) {described_class.new}
+  let(:station) {double :station}
+
+  describe '#start' do
+    it 'assigns an entry station' do
+      journey.start(station)
+      expect(journey.entry_station).to eq station
+    end
+  end
+
+end
