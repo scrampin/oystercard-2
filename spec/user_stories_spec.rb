d1f4383a7c58126215b4c70c478c751694145e79
Colin Turner <colinturner34@gmail.com>
2017-03-23 10:35:32 +0000

    Add user stories file

---
+++ spec/user_stories_spec.rb
@@ -0,0 +1,19 @@
+# In order to be correctly charged,
+# As a customer
+# Record entry station
+#
+#
+#
+# In order to be correctly charged,
+# As a customer,
+# Record exit station
+#
+#
+# In order to be correctly charged,
+# As a customer,
+# Calculate the correct fare
+#
+#
+# In order to be correctly charged,
+# As a customer,
+# Return whether or not journey is complete?
